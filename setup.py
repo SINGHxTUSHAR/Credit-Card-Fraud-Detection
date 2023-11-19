8b746965c1bbb18bbf5394c481b959f2ae097b01
TUSHAR SINGH <tusharsinghrawat.delhi@gmail.com>
2023-11-19 13:52:47 +0530

    updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="TUSHAR SINGH",
+    author_email="tusharsinghrawat.delhi@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
